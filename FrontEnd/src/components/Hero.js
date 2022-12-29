364e7f819e526c85d99a8eca6e625a10cdedde34
youssefrafaat <youssefhadrey@gmail.com>
2022-12-28 18:25:52 -0800

    home,about,hero,navbar

---
+++ FrontEnd/src/components/Hero.js
@@ -0,0 +1,17 @@
+import Sign from "../routes/SignUp";
+import "./Hero.css";
+function Hero(){
+    return(
+        <>
+        
+        <div className="hero">
+            <img alt="HerpImg"  src="ss.jpg"/>
+            <div className="hero-text">
+                <h1>Welcome To Travelio</h1>
+            </div>
+        </div>       
+        </>
+    );
+
+}
+export default Hero ;
