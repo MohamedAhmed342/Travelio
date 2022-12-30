0ba07f33dd42797823183c849a8f8bdbcd1be470
abdo1717 <abdoaeed48@gmail.com>
2022-12-30 22:45:17 +0200

    add package

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,31 +1,28 @@
-
 import './App.css';
-
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from './routes/Home';
 import Gallery from './routes/Gallery';
 import Offers from './routes/Offers';
 import Book from './routes/Book';
 import About from './routes/About';
-import SignUp from'./routes/SignUp';
-import Login from'./routes/Login';
+import SignUp from './routes/SignUp';
+import Login from './routes/Login';
 
 
 
- function App() {
+function App() {
   return (
     <div className="App">
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/Gallery' element={<Gallery />} />
-        <Route path='/Offers' element={<Offers />} />
+        <Route path='/offers' element={<Offers />} />
         <Route path='/Book' element={<Book />} />
         <Route path='/About' element={<About />} />
         <Route path='/SignUp' element={<SignUp />} />
         <Route path='/login' element={<Login />} />
-     </Routes>
-      
-     
+      </Routes>
+
     </div>
   );
 }
